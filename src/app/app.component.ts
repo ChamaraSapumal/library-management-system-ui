a40927bca7e1b6ca36588994c48efdad54e09d79
ChamaraSapumal <chamarasecu.21@gmail.com>
2024-04-12 22:52:01 +0530

    delete & update complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './common/navbar/navbar.component';
+import { AllBooksComponent } from './page/all-books/all-books.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    AllBooksComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
