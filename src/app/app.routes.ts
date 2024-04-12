2a4f91cde4ce2a4612d936c9318eef0672119c17
ChamaraSapumal <chamarasecu.21@gmail.com>
2024-04-13 00:41:05 +0530

    login and signup v1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './page/login/login.component';
+import { AllBooksComponent } from './page/all-books/all-books.component';
+import { RegisterComponent } from './page/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "login",
+        component: LoginComponent
+    },
+    {
+        path: "all-books",
+        component: AllBooksComponent
+    },
+    {
+        path: "signup",
+        component: RegisterComponent
+    }
+];
