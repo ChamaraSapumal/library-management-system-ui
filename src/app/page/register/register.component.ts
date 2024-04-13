5c365448f8caee1c041faba3bc62af90cf42a609
ChamaraSapumal <chamarasecu.21@gmail.com>
2024-04-13 12:03:08 +0530

    login and signup v2

--- src/app/page/register/register.component.ts
+++ src/app/page/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
@@ -15,6 +16,18 @@ export class RegisterComponent implements OnInit {
   private http;
   public countryList: any;
   public selectedCountry: any;
+  public selectedCountryCode: any;
+  public userObj = {
+    firstName: null,
+    lastName: null,
+    userName: null,
+    email: null,
+    address: null,
+    address2: null,
+    country: null,
+    contactNumber: null
+  }
+  public isExistsUser: any;
 
   constructor(private httpClient: HttpClient) {
     this.http = httpClient;
@@ -36,8 +49,42 @@ export class RegisterComponent implements OnInit {
 
   setSelectedCountry(country: any) {
     this.selectedCountry = country;
+    this.selectedCountryCode = country.idd.root + country.idd.suffixes[0];
     console.log(this.selectedCountry);
 
   }
 
+  submitForm() {
+    let apiUrl = "http://localhost:8081/reader/isExist/" + this.userObj.userName;
+
+    console.log(this.userObj);
+    this.http.get(apiUrl).subscribe(data => {
+      console.log(data);
+      this.isExistsUser = data;
+      this.registerUser(this.isExistsUser);
+    })
+  }
+
+  registerUser(isExistsUser: any) {
+    if (!isExistsUser == true) {
+      this.http.post("http://localhost:8081/reader/add-reader", this.userObj)
+        .subscribe(data => {
+          Swal.fire({
+            title: "Good job!",
+            text: "Registration Successful",
+            icon: "success",
+            showConfirmButton: false,
+            timer: 2000
+          });
+        })
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: `Can not register ${this.userObj?.firstName}!`,
+        footer: `username already has been taken`
+
+      });
+    }
+  }
 }
