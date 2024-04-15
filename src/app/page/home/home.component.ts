5f4aca1e659fe7590f79e3d2228a1b052628bcd9
ChamaraSapumal <chamarasecu.21@gmail.com>
2024-04-15 08:11:03 +0530

    home page v2

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../common/navbar/navbar.component";
+import { NgxTypedJsModule } from 'ngx-typed-js';
+import { CommonModule } from '@angular/common';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
-  imports: [NavbarComponent]
+  imports: [NavbarComponent, NgxTypedJsModule, CommonModule, RouterLink]
 })
 export class HomeComponent {
 
